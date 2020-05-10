38603169f5e09100f1931ced827d6b8fb5641ced
Laury Bueno <laury.bueno@gmail.com>
2020-05-10 18:20:02 -0300

    WIP: base 3.7 version using pybombs

---
+++ activate.sh
@@ -0,0 +1,10 @@
+
+xhost +local:docker
+
+docker run -it \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=unix$DISPLAY \
+    --device /dev/dri \
+    --device /dev/snd \
+    --device /dev/bus/usb \
+    laury/gnuradio:3.7 gnuradio-companion
